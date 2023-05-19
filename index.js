445936e6ed27cb63ed62576d6d48a8e37c57c56b
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-05-20 00:03:40 +0600

    Created Api for All toys

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
 			res.send(result);
 		});
 
+		app.get('/alltoys', async (req, res) => {
+			const result = await toyCollection.find().limit(20).toArray();
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db('admin').command({ ping: 1 });
 		console.log(
